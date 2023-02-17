65aa9e4fbd0886d32c3b4301625559cac666827f
xhr <2435540531@qq.com>
2023-02-17 15:53:40 +0800

    first commit

---
+++ git-test/src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.atguigu.git;
+
+/**
+ * @param: 代码
+ * @ClassName: GitTest
+ * @version: 1.0
+ * @description:
+ * @author: 刘原辰
+ * @CreateDate: 2023/2/17 15:48
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
