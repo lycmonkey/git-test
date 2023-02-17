ea947b4cc5d2fbd701bbd45e892c2d453bea6858
xhrFindCrane <2435540531@qq.com>
2023-02-17 12:17:23 +0000

    update git-test/src/main/java/com/atguigu/git/GitTest.java.
    Signed-off-by: xhrFindCrane <2435540531@qq.com>

--- git-test/src/main/java/com/atguigu/git/GitTest.java
+++ git-test/src/main/java/com/atguigu/git/GitTest.java
@@ -17,10 +17,13 @@ public class GitTest {
         System.out.println("master test!");
         System.out.println("hot-fix test!");
         System.out.println("push test!");
-        System.out.println("pull test!");
-        System.out.println("pull to gitee test!");
-        System.out.println("pull to gitee test!");
-        System.out.println("pull to gitee test!");
-        System.out.println("pull to gitee test!");
+        System.out.println("push test!");
+        System.out.println("push to gitee test!");
+        System.out.println("push to gitee test!");
+        System.out.println("push to gitee test!");
+        System.out.println("push to gitee test!");
+        System.out.println("pull from gitee test!");
+        System.out.println("pull from gitee test!");
+        System.out.println("pull from gitee test!");
     }
 }
