de149d6b03bcd10f02dbef7a4778849749f9a454
xhr <2435540531@qq.com>
2023-02-18 12:34:21 +0800

    line1

--- git-test/src/main/java/com/atguigu/git/GitTest.java
+++ git-test/src/main/java/com/atguigu/git/GitTest.java
@@ -11,24 +11,5 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
-        System.out.println("hello git2!");
-        System.out.println("hello git3!");
-        System.out.println("hello git4!");
-        System.out.println("master test!");
-        System.out.println("hot-fix test!");
-        System.out.println("push test!");
-        System.out.println("pull test!");
-        System.out.println("push to gitee test!");
-        System.out.println("push to gitee test!");
-        System.out.println("push to gitee test!");
-        System.out.println("push to gitee test!");
-        System.out.println("push test!");
-        System.out.println("push to gitee test!");
-        System.out.println("push to gitee test!");
-        System.out.println("push to gitee test!");
-        System.out.println("push to gitee test!");
-        System.out.println("pull from gitee test!");
-        System.out.println("pull from gitee test!");
-        System.out.println("pull from gitee test!");
     }
 }
